1a09cf6f67d8c88d181039fa105f4d0e3818d949
hangbiaozhou <111854044+hangbiaozhou@users.noreply.github.com>
2022-08-23 17:38:17 +0800

    Update test.java

--- src/main/java/com/example/test.java
+++ src/main/java/com/example/test.java
@@ -9,5 +9,6 @@ public class test {
         System.out.println("master");
         System.out.println("hot-fix-test");
         System.out.println("hot-fix-test2");
+         System.out.println("hot-fix-test3");
     }
 }
